39bae64e5b8980bc79f9ce85036b13ba90e69458
boboeva1118@gmail.com <boboeva1118@gmail.com>
2021-02-03 17:22:16 +0000

    Добавьте файлы проекта.

---
+++ dz02.02.2021/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace dz02._02._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Id = Convert.ToInt32(Console.ReadLine());
+            string FirstName = Console.ReadLine();
+            string LastName = Console.ReadLine();
+            string MiddleName = Console.ReadLine();
+            string  FavoritSport = Console.ReadLine();
+            Participants one = new Participants( Id, FirstName,  LastName,  MiddleName,   FavoritSport);
+            one.Participant();
+        }
+    }
+    class Participants
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public string MiddleName;
+        public string FavoritSport;
+       
+        public Participants (int Id, string FirstName, string LastName, string MiddleName, string FavoritSport )
+        {
+            this.Id = Id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.MiddleName = MiddleName;
+            this.FavoritSport = FavoritSport;
+
+        }
+        public void Participant()
+        {
+            Console.WriteLine($"Id: {Id},\n FirstName:{FirstName},\n LastName:{LastName},\nMiddleName: {MiddleName},\n FavoritSport:{FavoritSport} ");
+        }
+    }
+}
